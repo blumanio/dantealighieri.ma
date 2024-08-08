7ea065adb6bbf20c654ec3b3c84c804a8b6e566d
Mohamed El Aammari <medlique@gmail.com>
2024-08-08 15:20:51 +0200

    update vercel next config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,25 +6,32 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: '/:path*',
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          // replace this with your actual origin
-
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-          },
           {
             key: 'Access-Control-Allow-Origin',
-            value: '*'
+            value: 'https://dantealighieri.ma'
           },
           {
             key: 'Access-Control-Allow-Methods',
             value: 'GET,DELETE,PATCH,POST,PUT,OPTIONS'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
           }
         ]
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination:
+          'https://backend-jxkf29se8-mohamed-el-aammaris-projects.vercel.app/:path*'
       }
     ]
   }
