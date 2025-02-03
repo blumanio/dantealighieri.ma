af9383970601beb601a61b5bbf26e95faa436b4e
mohamed <medlique@gmail.com>
2025-02-03 09:10:32 +0100

    add project data

--- client/lib/data.ts
+++ client/lib/data.ts
@@ -16,7 +16,32 @@ export const askedQuestions = [
     answer: 'i am using latest in cutting edge technology conerning MERN Stack'
   }
 ]
-
+export const projectsData = [
+  {
+    id: 1,
+    image: '/projects/01.png',
+    category: 'Front-end',
+    title: 'Figma to react/nextjs',
+    description:
+      'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested'
+  },
+  {
+    id: 2,
+    image: '/projects/02.png',
+    category: 'Full-stack',
+    title: ' Github API',
+    description:
+      'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested'
+  },
+  {
+    id: 3,
+    image: '/projects/03.png',
+    category: 'Full-stack',
+    title: ' Graphql By example',
+    description:
+      'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested'
+  }
+]
 
 export const italianUniversities = [
   {
