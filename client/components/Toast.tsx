f140142da1d0e64a0b6a4b62ba36d6df4d4aa55a
mohamed <medlique@gmail.com>
2026-02-09 12:27:48 +0100

    sticky mobile button

--- client/components/Toast.tsx
+++ client/components/Toast.tsx
@@ -12,9 +12,11 @@ const WorkInProgressHeader = () => {
   return (
 
     <div className="fixed bottom-0 left-0 right-0 p-4 bg-white/80 backdrop-blur-md border-t border-neutral-200 z-50 md:hidden">
+      <a href='https://docs.google.com/spreadsheets/d/1aFVQkJeatoEE16MCv4ZNX374QYK3d4PY/edit?gid=2095688478#gid=2095688478'>
       <button className="w-full py-4 bg-primary text-white font-black rounded-xl shadow-lg flex items-center justify-center gap-2">
-        GET THE CHECKLIST — FREE
+        GET ALL UNIVERSITIES
       </button>
+      </a>
     </div>
 
   );
